10dc2c9e14303a8fece073fd9d4322547fad1312
Douglas Leal <douglas.leal@live.com>
2023-03-09 17:39:57 -0300

    Tirando fotos com o image-picker

--- lib/pages/form_page.dart
+++ lib/pages/form_page.dart
@@ -1,4 +1,8 @@
+import 'package:agenda_contatos/models/contato.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:async';
 
 class FormPage extends StatefulWidget {
   const FormPage({Key? key}) : super(key: key);
@@ -8,34 +12,77 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _telefoneController = TextEditingController();
+
+  final ImagePicker _picker = ImagePicker();
+
+  var imagem = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Contatos"),
+        title: const Text("Novo Contato"),
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.save),
-        onPressed: () {},
+        onPressed: () {
+          var contato = Contato.fromMap({
+            "nome": _nomeController.text,
+            "email": _emailController.text,
+            "telefone": _telefoneController.text,
+            "imagem": imagem
+          });
+
+          Navigator.pop(context, contato);
+        },
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            Icon(Icons.person, size: 128,),
-            TextField(decoration: InputDecoration(
-              labelText: "Nome"
-            ),),
-            TextField(decoration: InputDecoration(
-                labelText: "Email"
-            ),),
-            TextField(decoration: InputDecoration(
-                labelText: "Phone"
-            ),),
+            _imagem(),
+            _txtField(label: "Nome", controller: _nomeController),
+            _txtField(label: "Email", controller: _emailController),
+            _txtField(label: "Telefone", controller: _telefoneController),
           ],
         ),
       ),
     );
   }
+
+  _imagem(){
+    return GestureDetector(
+      child: imagem == "" ? const Icon(Icons.person, size: 128,) : Container(
+          width: 140.0,
+          height: 140.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+                image: FileImage(File(imagem)),
+                fit: BoxFit.cover
+            ),
+          ),),
+      onTap: () async{
+        print("testando...");
+        final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+        if(photo == null) return;
+        setState(() {
+          imagem = photo.path;
+        });
+      },
+    );
+  }
+
+  _txtField({label, controller}){
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label
+    ),);
+  }
+
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:agenda_contatos/models/contato.dart';
 import 'package:agenda_contatos/pages/form_page.dart';
 import 'package:flutter/material.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,6 +13,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<Contato> contatos = [
+    Contato.fromMap({"nome": "Douglas", "email": "douglas@email.com",
+      "telefone": "1234-5678",
+      "imagem": ""
+    }),
+    Contato.fromMap({"nome": "Ana", "email": "ana@email.com",
+      "telefone": "1234-5678",
+      "imagem": "imagem"
+    }),
+    Contato.fromMap({"nome": "Pedro", "email": "pedro@email.com",
+      "telefone": "1234-5678",
+      "imagem": ""
+    }),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +38,45 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => FormPage()),);
+        onPressed: () async {
+          var contato = await Navigator.push(context, MaterialPageRoute(builder: (context) => FormPage()),);
+
+          if(contato != null){
+            setState(() {
+              contatos.add(contato);
+            });
+          }
         },
       ),
-
+      body: ListView.builder(
+          itemCount: contatos.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListTile(
+                  leading: contatos[index].imagem == "" ? const Icon(Icons.person, size: 64,) : Container(
+                    width: 64,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: FileImage(File(contatos[index].imagem!)),
+                          fit: BoxFit.cover
+                      ),
+                    ),),
+                  title: Text(contatos[index].nome!),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(contatos[index].email!),
+                      Text(contatos[index].telefone!),
+                  ],),
+                ),
+              ),
+            );
+      }),
     );
+
   }
 }
