24b52907c024401e271c9a3e389b2ca2b2faf67e
Douglas Leal <douglas.leal@live.com>
2023-03-10 13:50:01 -0300

    Ligar e exclusão funcionando

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:agenda_contatos/Dao/contato_dao.dart';
 import 'package:agenda_contatos/models/contato.dart';
 import 'package:agenda_contatos/pages/form_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,7 +14,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final ContatoDao _dao = ContatoDao();
 
   List<Contato> contatos = [];
@@ -44,9 +43,12 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () async {
-          var contato = await Navigator.push(context, MaterialPageRoute(builder: (context) => FormPage()),);
+          var contato = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FormPage()),
+          );
 
-          if(contato != null){
+          if (contato != null) {
             await _dao.criar(contato);
             listarContatos();
           }
@@ -55,32 +57,115 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
           itemCount: contatos.length,
           itemBuilder: (context, index) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ListTile(
-                  leading: contatos[index].imagem == "" ? const Icon(Icons.person, size: 64,) : Container(
-                    width: 64,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: FileImage(File(contatos[index].imagem!)),
-                          fit: BoxFit.cover
+            return GestureDetector(
+                onLongPress: () {
+                  _opcoesContatos(context, index);
+                },
+                child: _cardContatos(index));
+          }),
+    );
+  }
+
+  _opcoesContatos(context, index) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return BottomSheet(
+          onClosing: () {},
+          builder: (context) {
+            return Container(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextButton(
+                      onPressed: () {
+                        final Uri launchUri =
+                            Uri(scheme: 'tel', path: contatos[index].telefone);
+
+                        launchUrl(launchUri);
+
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        "Ligar",
+                        style: TextStyle(color: Colors.red, fontSize: 16.0),
                       ),
-                    ),),
-                  title: Text(contatos[index].nome!),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(contatos[index].email!),
-                      Text(contatos[index].telefone!),
-                  ],),
-                ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+
+
+                      },
+                      child: const Text(
+                        "Editar",
+                        style: TextStyle(color: Colors.red, fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextButton(
+                      onPressed: () {
+                        _dao.excluir(contatos[index].id!);
+
+                        setState(() {
+                          listarContatos();
+                        });
+
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        "Excluir",
+                        style: TextStyle(color: Colors.red, fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             );
-      }),
+          },
+        );
+      },
     );
+  }
 
+  _cardContatos(index) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListTile(
+          leading: contatos[index].imagem == ""
+              ? const Icon(
+                  Icons.person,
+                  size: 64,
+                )
+              : Container(
+                  width: 64,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: FileImage(File(contatos[index].imagem!)),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+          title: Text(contatos[index].nome!),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(contatos[index].email!),
+              Text(contatos[index].telefone!),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
